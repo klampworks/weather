d5e04e948300b0beff0e6ac4be5df2554986179b
yukipyon <wolfvbear@gmail.com>
2014-01-29 19:14:42 +0000

    Added date field and removed uneeded constructor.

--- weather_day.hpp
+++ weather_day.hpp
@@ -5,15 +5,7 @@
 
 struct weather_day {
 
-	std::string temp, desc, url;
-
-	weather_day(const std::string &temp_,
-		const std::string &desc_,
-		const std::string &url_) :
-			temp(temp_),
-			desc(desc_),
-			url(url_) {}
-	
+	std::string date, temp, desc, url;
 	weather_day() {}
 };
 #endif
