1a1934fd326a0d1f0f9963fe461e68b1d3c1afbc
yukipyon <wolfvbear@gmail.com>
2014-01-29 18:41:25 +0000

    Removed dead code.

--- parser.cpp
+++ parser.cpp
@@ -8,7 +8,9 @@
 using namespace boost;
 using namespace property_tree;;
 
-void print(boost::property_tree::ptree const& pt)
+/* Debug function for recursivly printing out a node and its
+ * children. */
+void print(const boost::property_tree::ptree &pt)
 {
     using boost::property_tree::ptree;
     ptree::const_iterator end = pt.end();
@@ -54,56 +56,17 @@ int main()
 	while(std::getline(ifs, tmp)) 
 		ss << tmp;
 
-	//ss << "{ \"root\": { \"values\": [1, 2, 3, 4, 5 ] } }";
-
 	ptree pt;
 	read_json(ss, pt);
 
-	//print(pt);
-	//auto a = pt.get_child("data.current_condition..weatherDesc..");
-	//std::cout  << a.begin()->second.data() << std::endl;
-
 	auto a = pt.get_child("data.current_condition.");
 	parse_weather_day(a.begin()->second);
-	//a = pt.get_child("data.current_condition..weatherIconUrl..");
-	//std::cout  << a.begin()->second.data() << std::endl;
 
-	/* Yes the two dots at the end are fucking important. */
 	for (ptree::value_type &v: pt.get_child("data.weather.")) {
 
 		parse_weather_day(v.second);
 		
-#if 0
-		/* TODO It's late and I don't give a shit about
-		   learning this retarded API.*/
-		if (v.first.data() == std::string("temp_C")) {
-			std::cout << v.second.data() << std::endl;
-		}
-
-/*
-		if (v.first.data() == std::string("weatherDesc")) {
-			std::cout << 
-				v.second.get_child(".value").begin()->first.data() 
-				<< std::endl;
-		}
-		if (v.first.data() == std::string("weatherIconUrl")) {
-			std::cout << 
-				v.second.get_child(".value").begin()->first.data() 
-				<< std::endl;
-		}
-		*/
-
-		for (const auto &b: v.second) {
-			std::cout << b.first.data() << std::endl;
-		}
-
-
-//weatherIconUrl
-#endif
-
 	}
-
-
 
 	return EXIT_SUCCESS;
 }
