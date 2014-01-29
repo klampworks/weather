76b5b36c83450c9639a8061b27b457a710e46fec
yukipyon <wolfvbear@gmail.com>
2014-01-29 19:10:52 +0000

    Added a parser test.

---
+++ parser_test.cpp
@@ -0,0 +1,19 @@
+#include "parser.hpp"
+#include "weather_day.hpp"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	auto res = parser::parse_file("input");
+
+	for (const auto &r: res) {
+		std::cout <<
+			"\ntemp = " << r.temp <<
+			"\ndesc = " << r.desc <<
+			"\nurl  = " << r.url  <<
+			std::endl;
+	}
+
+	return 0;
+}
