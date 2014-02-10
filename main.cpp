17f37e3b28f16cc6691dc4be19288b68dad86d6a
yukipyon <wolfvbear@gmail.com>
2014-02-10 18:47:41 +0000

    Added post code as a command line argument.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 
 int main(int argc, char **argv) 
 {
-	assert(argc == 2);
+	assert(argc == 3);
 	QApplication app(argc, argv);
-	plane window(argv[1]);
+	plane window(argv[1], argv[2]);
 	window.show();
 	return app.exec();
 }
